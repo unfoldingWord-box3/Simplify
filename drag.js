02453c9393e8f43874b75d6f0e4b40649cfd81f0
Bruce Spidel <bruce.spidel@gmail.com>
2023-08-22 17:28:24 -0400

    Create drag.js

---
+++ drag.js
@@ -0,0 +1,44 @@
+
+  function dragElement( subject ) {          /** Move selected element anywhere on page */
+    // See init for required initialization
+    var x1, y1, x2, y2, dx;
+    
+    if( document.getElementById( subject.id + "header" ) ) {
+      // if present, the header is where you move the DIV from:
+      document.getElementById( subject.id + "header" ).onmousedown = dragMouseDown;
+      dx = 0;
+    } else {
+      // otherwise, move the DIV from anywhere inside the DIV:
+      subject.onmousedown = dragMouseDown;
+      dx = 0;
+    }
+
+    function dragMouseDown( e ) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      x2 = e.clientX;
+      y2 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+
+    function elementDrag( e ) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      x1 = x2 - e.clientX;
+      y1 = y2 - e.clientY;
+      x2 = e.clientX;
+      y2 = e.clientY;
+      // set the element's new position:
+      subject.style.top  = ( subject.offsetTop  - y1 - dx ) + "px";
+      subject.style.left = ( subject.offsetLeft - x1 - dx ) + "px";
+    }
+
+    function closeDragElement() {  // stop moving when mouse button is released:
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
